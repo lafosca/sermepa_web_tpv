bc5f6c0fd8051df84793382e46777989c9e8a3d0
David Cortés <david.cortes.18@gmail.com>
2015-11-12 13:21:31 +0100

    Updated signature class

--- lib/sermepa_web_tpv/signature.rb
+++ lib/sermepa_web_tpv/signature.rb
@@ -7,19 +7,23 @@ module SermepaWebTpv
   class Signature
     def self.signature_256(order_id, merchant_secret_key, merchant_parameters)
       order_signature = order_signature(order_id, merchant_secret_key)
-      Base64.encode64(OpenSSL::HMAC.digest('sha256',order_signature,merchant_parameters))
+      Base64.encode64(OpenSSL::HMAC.digest('sha256',Base64.encode64(order_signature.to_s),merchant_parameters))
     end
 
     private
 
     def self.order_signature(order_id, merchant_secret_key)
-      cipher = OpenSSL::Cipher::Cipher.new('des-ede3')
-      cipher.encrypt
-      cipher.key = Base64.decode64(merchant_secret_key)
-      encrypted = cipher.update(order_id)
-      encrypted << cipher.final
-
-      return Base64.encode64(encrypted)
+        cipher = OpenSSL::Cipher::Cipher.new("des-ede3")
+        cipher.encrypt
+        key = Base64.decode64(merchant_secret_key)
+        iv = [0, 0, 0, 0, 0, 0, 0, 0].map { |e| e.chr }.join("")
+        cipher.key = key
+        cipher.iv = iv
+        encrypted = cipher.update(order_id)
+        encrypted << cipher.final
+        signature = encrypted
+        puts signature
+        return signature
     end
   end
 end
